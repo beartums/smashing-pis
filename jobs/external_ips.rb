a600e9e1ba6c83c887bb3d76e7aee29f75885056
unknown <beartums@griffithnet.com>
2020-07-17 07:30:58 -0400

    WIP hermometer widget

--- jobs/external_ips.rb
+++ jobs/external_ips.rb
@@ -1,21 +1,21 @@
 require 'net/ssh'
 
-SCHEDULER.every '60s' do
+SCHEDULER.every '60s', :first_in => 0 do
   targets = JSON.parse(ENV['SERVERS'])
-  Net::SSH.start('192.168.0.24', 'pi', :password => ENV['password']) do |ssh|
-    server = targets.find_by("name" => "GrifGateway")
-    res = [
-      {
+  server = targets.find {|s| s["name"] == "GrifGateway"}
+  res = []
+  Net::SSH.start(server["ip"], server['user_id'], :password => server['password']) do |ssh|
+
+    res << {
         label: 'sabnzbd',
         value: ssh.exec!('docker exec sabnzbd curl -s icanhazip.com').gsub("\n","")
-      },
-      {
+      }
+
+    res << {
         label: 'transmission',
         value: ssh.exec!('docker exec transmission curl -s icanhazip.com').gsub("\n","")
       }
-    ]
   end
     
   send_event('external_ips', { items: res })
-  puts(res )
 end
--- jobs/pi_deets.rb
+++ jobs/pi_deets.rb
@@ -1,27 +1,48 @@
 require 'net/ssh'
 require 'json'
+
+MPSTAT_CPU_IDLE = "mpstat -u 1 1| awk '{ if ($2==\"all\" || $3==\"all\") {print $NF}}'"
+TOP_CPU_IDLE = "top -n 1 | grep %Cpu\(s\) | awk '{print $8}'"
+IOSTAT_CPU_IDLE = "iostat -c 1 2 | sed -n 8p | awk '{ print $6}'"
+
+START_SAR = "sudo sar -o -u 20  >/dev/null 2>&1 &"
+SAR_CPU_STATS_60S = "sar -f -u -s $(date --date=\"@$(($(date +%s)-60))\" +%H:%M:%S) -e $(date +%H:%M:%S)"
+SAR_CPU_STATS_OBJ_FORMAT = " | sed -r -n '/(%|Average)/p' | sed -r ' s/\s{2,}/,/g'"
+SAR_CPU_STATS_CPU_IDLE = "sar -f -u -s $(date --date=\"@$(($(date +%s)-60))\" +%H:%M:%S) -e $(date +%H:%M:%S) | grep Average | awk '{ print $8 }'"
+
+SYS_TEMP_MILLICELSIUS = 'cat /sys/class/thermal/thermal_zone*/temp'
+
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '20s', :first_in => 0 do |job|
+SCHEDULER.every '30s', :first_in => 0 do |job|
   Constants::SERVERS.each do |server|
-    Net::SSH.start(server['ip'], server['user_id'], :password => server['password']) do |ssh|
-      temp = temperature(ssh)
-      pct_active = cpu_activity(ssh)
-      
-      send_event("#{server['name']}_cpu", { value: pct_active })
-      send_event("#{server['name']}_temp", { value: temp })
+    temp = nil
+    activity = nil
+    begin
+      Net::SSH.start(server['ip'], server['user_id'], :password => server['password']) do |ssh|
+        temp = pi_temperature(ssh)
+        activity = pi_cpu_activity(ssh)    
+      end
+      send_event("#{server['name']}_temp", temp.merge(moreinfo: server["ip"]))
+      send_event("#{server['name']}_cpu", activity.merge(moreinfo: server["ip"]))
+    rescue
+      send_event("#{server['name']}_temp", { moreinfo: server["ip"], status: 'offline' })
+      send_event("#{server['name']}_cpu", { moreinfo: server["ip"], status: 'offline' })
     end
   end
 end
 
-def temperature(ssh)
-  ssh.exec!('cat /sys/class/thermal/thermal_zone*/temp').gsub("\n","").to_i/1000
+def pi_temperature(ssh)
+  { value: ssh.exec!(SYS_TEMP_MILLICELSIUS).to_i/1000 }
 end
 
-def cpu_activity(ssh)
-  pct_idle = ssh.exec!("mpstat -P ALL| awk '{ if ($2==\"all\" || $3==\"all\") {print $NF}}'").to_f
-  begin
-    (100 - pct_idle).round(0)
-  rescue JSON::ParserError, NoMethodError
-    "N/A"
+def pi_cpu_activity(ssh)
+  result = {}
+  ssh.exec! SAR_CPU_STATS_CPU_IDLE do |ch, stream, data|
+    if stream == :stderr
+      result = { error: data, value: "N/A" }
+    else
+      result = { value: (100 - data.to_f).round(0) }
+    end
   end
+  result
 end
